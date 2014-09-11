b57878798bc7e7a8262a52e13689ab1d7f80a3a8
Boris Shurygin <borisshr@gmail.com>
2014-09-11 18:11:17 +0400

    Limited implementation of MyStack example task

--- example_task/sources/task/iface.h
+++ example_task/sources/task/iface.h
@@ -12,6 +12,40 @@ using namespace Utils;
  */
 namespace ExampleTask
 {
-    
+    // Stack of integers
+    class MyStack: public Printable<MyStack>
+    {
+    public:
+        typedef UInt32 Size;
+        
+        class Error: public std::exception
+        {
+        public:
+            virtual const char* what() const throw();
+        };
+
+        static const Size INIT_SIZE = 20;
+
+        MyStack();
+        ~MyStack();
+        void toStream( std::ostream &stream) const;
+        
+        int pop();
+        void push(int val);
+
+        inline Size size() const { return _size; }
+    private:
+        Size _size;
+        Size _impl_size;
+        int *mem_p;
+
+        void grow();
+        void shrink();
+
+        MyStack( const MyStack &orig);
+        MyStack &operator =( const MyStack &orig);
+    };
+
+
     bool uTest( UnitTest *utest_p);
 };
--- example_task/sources/task/my_stack.cpp
+++ example_task/sources/task/my_stack.cpp
@@ -2,8 +2,88 @@
 
 namespace ExampleTask
 {
+    const char* MyStack::Error::what() const throw()
+    {
+        return "stack internal error";
+    }
+
+    // Stack of integers
+    MyStack::MyStack(): 
+        _size(0),
+        mem_p( new int[INIT_SIZE]),
+        _impl_size( INIT_SIZE)
+    {
+    
+    }
+
+    MyStack::~MyStack()
+    {
+        delete[] mem_p;
+    }
+    
+    void MyStack::toStream( std::ostream &stream) const 
+    {}
+    
+    void MyStack::shrink()
+    {
+        _size--; // naive implementation
+    }
+
+    void MyStack::grow()
+    {
+        if ( _size + 1 >= _impl_size)
+            throw Error();
+
+        _size++; // naive implementation
+    }
+
+    int MyStack::pop()
+    { 
+        if ( size() == 0) throw Error();
+
+        int val = *(mem_p + size() - 1);
+
+        shrink();
+
+        return val;
+    }
+    
+    void MyStack::push(int val)
+    {
+        *(mem_p + size()) = val;
+
+        grow();
+    }
+        
     bool uTest( UnitTest *utest_p)
     {
+        bool thrown_error = false;
+
+        //Tested instance
+        MyStack stack;
+
+        stack.push(1);
+        stack.push(2);
+
+        UTEST_CHECK( utest_p, stack.size() == 2);
+
+        int second = stack.pop();
+        int first = stack.pop();
+
+        UTEST_CHECK( utest_p, second == 2);
+        UTEST_CHECK( utest_p, first == 1);
+        
+        UTEST_CHECK( utest_p, stack.size() == 0);
+
+        try
+        {
+            stack.pop();
+        } catch ( MyStack::Error)
+        {
+            thrown_error = true;
+        }
+        UTEST_CHECK( utest_p, thrown_error);
+
         return utest_p->result();
     }
 };
