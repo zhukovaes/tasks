1428878813fedc79829750c4bf76369cde3866e2
Boris Shurygin <borisshr@gmail.com>
2014-10-13 18:05:04 +0400

    removed tools dir

--- task1/sources/Tools/lib.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * @file: lib.cpp
- * Dummy file for library generation
- */
-/*
- * GraphImpl library, internal representation of GraphImpls in SCL (Simple Compiler) tool.
- * Copyright (C) 2012  Boris Shurygin
- */
-#include "tools_iface.h"
